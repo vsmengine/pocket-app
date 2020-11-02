0491ef323acf2f3bb6492fd4edd5bfe2d10be9d3
V.S.M <sampath.vithanage1@gmail.com>
2020-11-02 21:06:18 +0200

    setup download contact data

--- app/src/main/java/com/example/pocketapp/contact/GetContacts.java
+++ app/src/main/java/com/example/pocketapp/contact/GetContacts.java
@@ -1,15 +1,81 @@
 package com.example.pocketapp.contact;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pocketapp.R;
+import com.example.pocketapp.other.Tags;
+import com.example.pocketapp.services.DownloadService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GetContacts extends AppCompatActivity {
+  private ProgressDialog progressDialog = null;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_cont);
+
+    DownloadContacts downloadContacts = new DownloadContacts();
+    downloadContacts.execute();
   };
+
+  public class DownloadContacts extends AsyncTask<Void, Void, Void> {
+
+    public String downloadUrl = "https://api.androidhive.info/contacts/";
+    private ProgressDialog dialog;
+    private DownloadService downloadService;
+    private JSONArray contactsArray = null;
+//  ArrayList<HashMap<String, String>> contactList;
+
+    @Override
+    protected void onPreExecute() {
+      super.onPreExecute();
+      dialog = new ProgressDialog(GetContacts.this);
+      dialog.setTitle("Pocket App");
+      dialog.setMessage("contacts are downloading");
+      dialog.setIndeterminate(true);
+      dialog.setCancelable(false);
+      dialog.show();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+      super.onPostExecute(aVoid);
+      if(dialog.isShowing()){
+        dialog.dismiss();
+      }
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+      DownloadService service = new DownloadService();
+      String stringContacts = service.downloadServiceCall(downloadUrl);
+//      Log.d("Response",">" + stringContacts);
+      if(stringContacts != null) {
+        try {
+          JSONObject jsonContacts = new JSONObject(stringContacts);
+          contactsArray = jsonContacts.getJSONArray(Tags.TAG_CONTACTS);
+          for(int i = 0; i < contactsArray.length(); i++) {
+            JSONObject contact = contactsArray.getJSONObject(i);
+
+          }
+//          Log.d("Response",">" + jsonContacts);
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+      } else {
+
+      }
+      return null;
+    }
+  }
 }
---
+++ app/src/main/java/com/example/pocketapp/other/Tags.java
@@ -0,0 +1,14 @@
+package com.example.pocketapp.other;
+
+public class Tags {
+  public static final String TAG_CONTACTS = "contacts";
+  public static final String TAG_ID = "id";
+  public static final String TAG_NAME = "name";
+  public static final String TAG_EMAIL = "email";
+  public static final String TAG_ADDRESS = "address";
+  public static final String TAG_GENDER = "gender";
+  public static final String TAG_PHONE = "phone";
+  public static final String TAG_MOBILE = "mobile";
+  public static final String TAG_HOME = "home";
+  public static final String TAG_OFFICE = "office";
+}
